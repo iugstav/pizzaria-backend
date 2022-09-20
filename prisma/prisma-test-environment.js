042ba1ef5a8807b11b6f9852b3d08d0f80eb9d16
iugstav <soaresgu2710@gmail.com>
2022-09-20 00:15:44 -0300

    minor changes

--- prisma/prisma-test-environment.js
+++ prisma/prisma-test-environment.js
@@ -1,26 +1,32 @@
-const { randomUUID } = require("crypto")
-const { exec } = require('child_process')
-const NodeEnvironment = require('jest-environment-node').default
-const { Client } = require("pg")
-const util = require('util')
+const { randomUUID } = require("crypto");
+const { exec } = require('child_process');
+const NodeEnvironment = require('jest-environment-node').default;
+const { Client } = require("pg");
+const util = require('util');
 
 require("dotenv-flow").config({ node_env: 'test', silent: true });
 
 const execSync = util.promisify(exec);
 
-const prismaBinary = './node_modules/.bin/prisma'
+const prismaBinary = './node_modules/.bin/prisma';
 
 class PrismaTestEnvironment extends NodeEnvironment {
   constructor(config) {
     super(config);
 
-    this.schema = `test_${randomUUID()}`
-    this.connectionString = process.env.DATABASE_URL;
+    const user = process.env.DATABASE_USER;
+    const password = process.env.DATABASE_PASSWORD;
+    const databaseName = process.env.DATABASE_NAME;
+    const host = process.env.DATABASE_HOST;
+    const port = process.env.DATABASE_PORT;
+
+    this.schema = `test_${randomUUID()}`;
+    this.connectionString = `postgresql://${user}:${password}@${host}:${port}/${databaseName}?schema=${this.schema}`;
   }
 
   async setup() {
-    process.env.DATABASE_URL = this.connectionString
-    this.global.process.env.DATABASE_URL = this.connectionString
+    process.env.DATABASE_URL = this.connectionString;
+    this.global.process.env.DATABASE_URL = this.connectionString;
 
     // Rodar as migrations
     await execSync(`${prismaBinary} migrate deploy --preview-feature`);
@@ -31,14 +37,15 @@ class PrismaTestEnvironment extends NodeEnvironment {
   async teardown() {
     const client = new Client({
       connectionString: this.connectionString
-    })
+    });
 
-    await client.connect()
-    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`)
-    await client.end()
+    await client.connect();
+    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
+    await client.end();
 
+    return super.teardown();
   }
 }
 
-module.exports = PrismaTestEnvironment
+module.exports = PrismaTestEnvironment;
 
